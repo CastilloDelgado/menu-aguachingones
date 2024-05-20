de0e5ddc73d0ba069738664abeac6b27c03af07f
Marco Castillo <marcocastillo1997@gmail.com>
2024-05-19 21:09:11 -0600

    Finishing second version

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,13 @@
+<script setup>
+import logo from "../assets/shrimp-logo.png"
+</script>
+
 <template>
-    <div class="border-t-4 text-center border-primary-600 bg-white text-primary-600 py-4 grid place-content-center">
+    <div class="border-t-4 text-center bg-primary-600 text-white py-4 grid place-content-center">
+        <a href="#header" class="text-sm md:text-medium capitalize underline font-ligh mb-2">Volver arriba</a>
         <p class="  uppercase font-title"> Los Aguachingones</p>
-        <a href="#header" class="text-xs capitalize underline">Volver arriba</a>
+        <div class="flex justify-center mb-2">
+            <img :src="logo" alt="" class="w-8 h-8 shake">
+        </div>
     </div>
 </template>
