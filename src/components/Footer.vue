0d170d5bd74acb366f4cb870f4668d6a60431431
Marco Castillo <marcocastillo1997@gmail.com>
2024-05-19 21:22:52 -0600

    Removing border in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@ import logo from "../assets/shrimp-logo.png"
 </script>
 
 <template>
-    <div class="border-t-4 text-center bg-primary-600 text-white py-4 grid place-content-center">
+    <div class="text-center bg-primary-600 text-white py-4 grid place-content-center">
         <a href="#header" class="text-sm md:text-medium capitalize underline font-ligh mb-2">Volver arriba</a>
         <p class="  uppercase font-title"> Los Aguachingones</p>
         <div class="flex justify-center mb-2">
